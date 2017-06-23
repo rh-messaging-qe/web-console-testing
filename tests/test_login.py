0af8959d63d6fb0b95090136e4ee392b05b38d8b
Dominik Lenoch <dlenoch@redhat.com>
2017-06-23 11:51:59 +0200

    UPD(tests) login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,66 +1,86 @@
 import pytest
 from pages.login import LoginPage
+from pages.welcome import WelcomePage
 
+from os.path import dirname, join
+from pickle import dump
 
 @pytest.mark.nondestructive
-def test_login_valid(base_url: str, selenium, variables):
+def test_login_valid(base_url, selenium, user):
     """
     Users can log in
+    @param base_url:
+    @param selenium:
+    @param user: fixtures
     @type base_url: string
     @type selenium:
-    @type variables: strings
+    @type user:
     """
     login_page = LoginPage(base_url, selenium)
-    login_page.login(variables['username'], variables['password'])
+    welcome_page = login_page.login(user.username, user.password)
+    assert welcome_page.is_the_current_page
 
 
 @pytest.mark.nondestructive
-def test_login_invalid_password(base_url, selenium, variables):
+def test_login_invalid_password(base_url, selenium, user):
     """
     Users cannot login with wrong password
     @type base_url: string
     @type selenium:
-    @type variables: strings
+    @type user: strings
     """
     login_page = LoginPage(base_url, selenium)
-    login_page.login(variables['username'], variables['password'] + 'x')
-    assert login_page.is_forbidden
+    login = login_page.login(user.username, user.password + 'x')
+    assert login_page.is_the_current_page
+    #assert login_page.is_forbidden
 
 
 @pytest.mark.nondestructive
-def test_login_invalid_username(base_url, selenium, variables):
+def test_login_invalid_username(base_url, selenium, user):
     """
     Attempt to log in with an invalid username
     @type base_url: string
     @type selenium:
-    @type variables: strings
+    @type user: strings
     """
     login_page = LoginPage(base_url, selenium)
-    login_page.login(variables['username'] + 'x', variables['password'])
-    assert login_page.is_forbidden
-
+    login = login_page.login(user.username + 'x', user.password)
+    assert login_page.is_the_current_page
+    #assert login_page.is_forbidden
 
 @pytest.mark.nondestructive
-def test_login_invalid_password_username(base_url, selenium, variables):
+def test_login_invalid_password_username(base_url, selenium, user):
     """
     Users cannot login with wrong password
     @type base_url: string
     @type selenium:
-    @type variables: strings
+    @type user: strings
     """
     login_page = LoginPage(base_url, selenium)
-    login_page.login(variables['username'] + 'x', variables['password'] + 'x')
-    assert login_page.is_forbidden
-
+    login = login_page.login(user.username + 'x', user.password + 'x')
+    assert login_page.is_the_current_page
+    #assert login_page.is_forbidden
 
 @pytest.mark.nondestructive
-def test_logout(base_url, selenium, variables):
+def test_logout(base_url, selenium, user):
     """
     Users can logout
     @type base_url: string
     @type selenium:
-    @type variables: strings
+    @type user: strings
     """
     login_page = LoginPage(base_url, selenium)
-    login_page.login(variables['username'], variables['password'])
-    assert 'welcome' in login_page.get_url_current_page()
+    welcome_page = login_page.login(user.username, user.password)
+    assert welcome_page.is_the_current_page
+    #welcome.Menu.click_logout()
+    #assert login_page.is_the_current_page
+
+@pytest.mark.nondestructive
+def test_logon_fixture(base_url, selenium, user, setup_login):
+    """
+    Users can logout
+    @type base_url: string
+    @type selenium:
+    @type user: strings
+    """
+    assert WelcomePage.is_the_current_page
